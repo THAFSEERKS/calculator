8937c7e3b71864289cc34f8d21694855327c45d6
THAFSEERKS <muhammedthafseer.mec@gmail.com>
2024-07-09 22:24:07 -0400

    first commit

---
+++ calc.js
@@ -0,0 +1,16 @@
+var screen = document.getElementById('screen');
+
+function buttonclick(value){
+    screen.value +=value;
+
+}
+
+function clearscreen() {
+    screen.value= "";
+    
+}
+
+function result(){
+    var result = eval(screen.value)
+    screen.value = result;
+}
